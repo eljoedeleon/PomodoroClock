7fc6b8fd7f88364f593e5990184f210fac58d6af
eljoedeleon <deliseo316@gmail.com>
2020-09-11 00:45:19 +0800

    Updates

---
+++ precache-manifest.261f4dc561a70b5328f89533d64333a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ddddb03edc2b6904215287f6a7570c2",
+    "url": "/PomodoroClock/index.html"
+  },
+  {
+    "revision": "e63d0a1c16e047b38b6b",
+    "url": "/PomodoroClock/static/css/main.b7db2205.chunk.css"
+  },
+  {
+    "revision": "291d4f8c19cd1a787a3b",
+    "url": "/PomodoroClock/static/js/2.d46e7d74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PomodoroClock/static/js/2.d46e7d74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e63d0a1c16e047b38b6b",
+    "url": "/PomodoroClock/static/js/main.207dcb03.chunk.js"
+  },
+  {
+    "revision": "4c780b8b07b108bd175b",
+    "url": "/PomodoroClock/static/js/runtime-main.a0fb29f6.js"
+  }
+]);
